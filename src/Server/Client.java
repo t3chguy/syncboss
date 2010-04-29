068f1fc32c62f8afcbb2dda692ff2a852134598a
youknowjack@gmail.com <youknowjack@gmail.com@3e462ed8-b18d-11de-a210-8fd6ca7157a7>
2010-04-29 11:01:42 +0000

    working nice +-20ms, still need to save configs

--- src/Server/Client.java
+++ src/Server/Client.java
@@ -157,15 +157,17 @@ public class Client implements UDPHandler, AbstractMediaPlayer, TCPHandler {
     long outBufHead = 0;
 
     public void sendDataMessage(final byte[] msg) throws Exception {
+        sendDataMessage(msg, msg.length);
+    }
 
-        if ((outBufHead + msg.length - outBufWrite) > outBuf.length) {
+    public void sendDataMessage(final byte[] msg, int len) throws Exception {
+        if ((outBufHead + len - outBufWrite) > outBuf.length) {
             throw new Exception("Buffer overflow in Server/Client.java for client "+ipString+", aborting send\n");
         }
-        for (int i = 0; i < msg.length; i++) {
+        for (int i = 0; i < len; i++) {
             outBuf[(int) (outBufHead % outBuf.length)] = msg[i];
             outBufHead++;
         }
-
     }
 
     private void sendBuffer() throws InterruptedException {
@@ -195,7 +197,7 @@ public class Client implements UDPHandler, AbstractMediaPlayer, TCPHandler {
         try { //todo: retry?
             sendDataMessage("p\n");
         } catch (Exception e) {
-            System.out.println(e.toString());
+            e.printStackTrace();
         }
     }
 
@@ -203,14 +205,16 @@ public class Client implements UDPHandler, AbstractMediaPlayer, TCPHandler {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
+    byte[] sbytes;
+    byte[] send = new byte[MediaTransmitter.getPacketSize()*2]; //no idea if this is the right size TODO
     public void queue(byte[] data, long time) {
         try {
             String s = "d" + (time + offset) + "\n";
-            byte[] sbytes = s.getBytes();
-            byte[] send = new byte[sbytes.length + data.length];
+            sbytes = s.getBytes();
+            //send = new byte[sbytes.length + data.length];
             System.arraycopy(sbytes,0,send,0,sbytes.length);
             System.arraycopy(data,0,send,sbytes.length,data.length);
-            sendDataMessage(send);
+            sendDataMessage(send, sbytes.length + data.length);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -220,7 +224,7 @@ public class Client implements UDPHandler, AbstractMediaPlayer, TCPHandler {
         try { //todo: retry?
             sendDataMessage(getFormatString(format));
         } catch (Exception e) {
-            System.out.println(e.toString());
+            e.printStackTrace();
         }
     }
 
